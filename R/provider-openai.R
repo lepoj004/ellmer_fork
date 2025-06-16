4982681a28d84548f8a24a52bcfb21df03f8ca53
lepoj004 <james.f.lepore@disney.com>
2025-06-16 14:53:36 -0700

    Update provider-openai.R

--- R/provider-openai.R
+++ R/provider-openai.R
@@ -154,7 +154,7 @@ method(chat_path, ProviderOpenAI) <- function(provider) {
 # https://platform.openai.com/docs/api-reference/chat/create
 method(chat_body, ProviderOpenAI) <- function(
   provider,
-  stream = FALSE,
+  stream = TRUE,
   turns = list(),
   tools = list(),
   type = NULL
